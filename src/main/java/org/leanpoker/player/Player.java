43b67b2e9ffa5198ebfa9bbf97192c78b1274f55
Pawel Krupa <pawel.krupa.junior@gmail.com>
2018-07-23 15:16:36 +0200

    Update Player.java

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -8,7 +8,6 @@ import org.leanpoker.cards.CardUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Player {
 
@@ -19,18 +18,23 @@ public class Player {
         JsonObject requestObject = request.getAsJsonObject();
         JsonElement ourPlayer = requestObject.get("players").getAsJsonArray().get(playerId);
         JsonArray ourCards = ourPlayer.getAsJsonObject().get("hole_cards").getAsJsonArray();
-        List<Card> cards = setHoleCards(ourCards);
+        JsonArray jsonCommunityCards = requestObject.get("community_cards").getAsJsonArray();
+        List<Card> holeCards = setCards(ourCards);
+        List<Card> communityCards = setCards(jsonCommunityCards);
 //        int pot = requestObject.get("pot").getAsInt();
         int bet = ourPlayer.getAsJsonObject().get("bet").getAsInt();
         int minimumRaise = requestObject.get("minimum_raise").getAsInt();
         int currentBuyIn = requestObject.get("current_buy_in").getAsInt();
 
-        int maxBetValue = CardUtils.checkCards(cards);
+        int maxBetValue = CardUtils.checkCards(holeCards);
 
         if (bet + minimumRaise <= maxBetValue) {
             if (currentBuyIn == bet && bet != 0) {
                 System.out.println("Check");
                 return 0;
+            }
+            if (communityCards.size() > 0) {
+                return currentBuyIn - bet + minimumRaise;
             }
             return currentBuyIn - bet;
         }
@@ -40,7 +44,7 @@ public class Player {
     public static void showdown(JsonElement game) {
     }
 
-    public static List<Card> setHoleCards(JsonArray playerCards) {
+    public static List<Card> setCards(JsonArray playerCards) {
         List<Card> holeCards = new ArrayList<>(2);
         for (JsonElement jsonCard : playerCards) {
             String rank = jsonCard.getAsJsonObject().get("rank").getAsString();
