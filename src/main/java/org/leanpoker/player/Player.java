73e9f537591039f3ba3f81a0799b419fc072d035
Pawel Krupa <pawel.krupa.junior@gmail.com>
2018-07-23 11:27:09 +0200

    Merge branch 'master' of git@github.com:empyty/poker-player-cashino.git
    Conflicts:
    	src/main/java/org/leanpoker/player/Player.java

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,11 @@ public class Player {
     static final String VERSION = "Testing JSON objects - players";
 
     public static int betRequest(JsonElement request) {
+<<<<<<< HEAD
         System.out.println(request.getAsJsonObject().get("players").getAsJsonArray().get(4));
+=======
+        System.out.println(request.getAsJsonObject().get("players").getAsJsonArray());
+>>>>>>> master
         System.out.println(request.getAsJsonObject());
         return 10;
     }
