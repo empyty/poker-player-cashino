049368ad94955ffd350482a9e99a1be34405952b
Pawel Krupa <pawel.krupa.junior@gmail.com>
2018-07-23 11:57:26 +0200

    Update Card.java and Player.java

---
+++ src/main/java/org/leanpoker/cards/Card.java
@@ -0,0 +1,50 @@
+package org.leanpoker.cards;
+
+import java.util.HashMap;
+
+public class Card {
+
+    public static final HashMap<String, Suit> suits;
+    public static final HashMap<String, Rank> ranks;
+    static {
+        suits = new HashMap<>(4);
+        ranks = new HashMap<>(13);
+        suits.put("spades", Suit.SPADES);
+        suits.put("hearts", Suit.HEARTS);
+        suits.put("diamonds", Suit.DIAMONDS);
+        suits.put("clubs", Suit.CLUBS);
+
+        ranks.put("A", Rank.ACE);
+        ranks.put("2", Rank.TWO);
+        ranks.put("3", Rank.THREE);
+        ranks.put("4", Rank.FOUR);
+        ranks.put("5", Rank.FIVE);
+        ranks.put("6", Rank.SIX);
+        ranks.put("7", Rank.SEVEN);
+        ranks.put("8", Rank.EIGHT);
+        ranks.put("9", Rank.NINE);
+        ranks.put("10", Rank.TEN);
+        ranks.put("J", Rank.JACK);
+        ranks.put("Q", Rank.QUEEN);
+        ranks.put("K", Rank.KING);
+    }
+
+    private Suit suit;
+    private Rank rank;
+    private int value;
+
+    public Card(String suit, String rank) {
+        this.suit = suits.get(suit);
+        this.rank = ranks.get(rank);
+    }
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return this.suit.toString() + this.rank.toString();
+    }
+}
--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,17 +1,33 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.leanpoker.cards.Card;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Player {
 
     static final String VERSION = "Testing JSON objects - players";
+    private static final int playerId = 4;
 
-    private String name;
+    private static String name;
+    private static List<Card> holeCards = new ArrayList<>();
 
     public static int betRequest(JsonElement request) {
-        System.out.println(request.getAsJsonObject().get("players").getAsJsonArray().get(4));
+        JsonObject requestObject = request.getAsJsonObject();
+        JsonElement ourPlayer = requestObject.get("players").getAsJsonArray().get(playerId);
+        JsonArray ourCards = ourPlayer.getAsJsonObject().get("hole_cards").getAsJsonArray();
+        for (JsonElement jsonCard : ourCards) {
+            String rank = jsonCard.getAsJsonObject().get("rank").getAsString();
+            String suit = jsonCard.getAsJsonObject().get("suit").getAsString();
+            Card card = new Card(suit, rank);
+            holeCards.add(card);
+            System.out.println(card);
+        }
         return 10;
     }
 
